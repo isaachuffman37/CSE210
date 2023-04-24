6b9f4aea30a6b2036c6ca04e84d6a9791d1eb1e1
Isaac Huffman <isaac.huffman99@gmail.com>
2023-04-24 11:54:32 -0600

    first fix

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //asked user for input
+        Console.Write("What is your first names? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}");
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter_grade = "";
+
+        if (percent >= 90)
+        {
+            letter_grade = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter_grade = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter_grade = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter_grade = "D";
+        }
+        else
+        {
+            letter_grade = "F";
+        }
+        Console.WriteLine($"Your grade is : {letter_grade}");
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Gotta try better next time.");
+        }
     }
 }
